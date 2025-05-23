8d9008364c34e6c460d2795798883b4ffa60c8cf
jakobch01 <151140167+jakobch01@users.noreply.github.com>
2025-05-23 12:24:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.use('/api/customers', require('./routes/customerRoutes'));
 
 // Start server
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
