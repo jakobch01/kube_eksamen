bb7f5860fbd2a409a409bdeed99c943647f9ab61
jakobch01 <151140167+jakobch01@users.noreply.github.com>
2025-06-01 21:02:32 +0200

    asdasd

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.use('/api/customers', require('./routes/customerRoutes'));
 // Start server
 const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server sdadsad is running on http://0.0.0.0:${PORT}`);
 });
